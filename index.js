c7578c890f26e81da7f927cee66bd4834e2929cd
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-09-19 14:06:37 -0300

    Agrego sección y funcionalidad de: categorias y Reportes.

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+const linkBalance = document.querySelector(".link-balance");
+const linkCategorias = document.querySelector(".link-categorias");
+const linkReportes = document.querySelector(".link-reportes");
 
-const linkBalance = document.querySelector(".link-balance")
-const linkCategorias = document.querySelector(".link-categorias")
-const linkReportes = document.querySelector(".link-reportes")
+const seccionPrincipal = document.querySelector(".principal");
+const seccionCategorias = document.querySelector(".categorias");
+const seccionReportes = document.querySelector(".reportes");
 
-const seccionPrincipal = document.querySelector(".principal")
-const seccionCategorias = document.querySelector(".categorias")
+linkBalance.onclick = (event) => {
+  event.preventDefault();
 
+  seccionPrincipal.style.display = "flex";
+  seccionReportes.style.display = "none";
+  seccionCategorias.style.display = "none";
+};
 
-linkBalance.onclick = (event) =>{
-    event.preventDefault();
-    
-    seccionPrincipal.style.display = "flex"
-   
-}
+linkCategorias.onclick = (event) => {
+  event.preventDefault();
+  seccionCategorias.style.display = "flex";
+  seccionPrincipal.style.display = "none";
+  seccionReportes.style.display = "none";
+};
 
-linkCategorias.onclick = (event) =>{
-    event.preventDefault();
-    seccionCategorias.style.display = "flex"
-    seccionPrincipal.style.display = "none"
-  
-}
-/*
-linkReportes.onclick = () =>{
-    l.style.display = "flex"
-}*/
+linkReportes.onclick = (event) => {
+  event.preventDefault();
+  seccionPrincipal.style.display = "none";
+  seccionCategorias.style.display = "none";
+  seccionReportes.style.display = "flex";
+};
