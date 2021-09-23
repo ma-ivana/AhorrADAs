d90380e86d068a871101c15cef84516a84ab0df7
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-09-23 16:48:07 -0300

    Agrego menu desplegable en nav mobile. Y su funcionalidad en js.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*//////////////////// Links del nav a secciones ////////////////////////*/
+
 const linkBalance = document.querySelector(".link-balance");
 const linkCategorias = document.querySelector(".link-categorias");
 const linkReportes = document.querySelector(".link-reportes");
@@ -23,6 +25,43 @@ linkCategorias.onclick = (event) => {
 };
 
 linkReportes.onclick = (event) => {
+  event.preventDefault();
+
+  seccionPrincipal.classList.add("is-hidden");
+  seccionCategorias.classList.add("is-hidden");
+  seccionReportes.classList.remove("is-hidden");
+};
+
+/*//////////////////// Menú desplegable mobile ////////////////////////*/
+
+const menuDesplegableMobile = document.getElementById("menu-desplegable-hamburguesa")
+const iconoHamburguesa = document.getElementById("icono-hamburguesa")
+
+const linkBalanceMobile = document.querySelector(".link-balance-mobile");
+const linkCategoriasMobile = document.querySelector(".link-categorias-mobile");
+const linkReportesMobile = document.querySelector(".link-reportes-mobile");
+
+iconoHamburguesa.onclick = () =>{
+  menuDesplegableMobile.classList.toggle("is-hidden")
+}
+
+linkBalanceMobile.onclick = (event) => {
+  event.preventDefault();
+
+  seccionPrincipal.classList.remove("is-hidden");
+  seccionCategorias.classList.add("is-hidden");
+  seccionReportes.classList.add("is-hidden");
+};
+
+linkCategoriasMobile.onclick = (event) => {
+  event.preventDefault();
+
+  seccionPrincipal.classList.add("is-hidden");
+  seccionCategorias.classList.remove("is-hidden");
+  seccionReportes.classList.add("is-hidden");
+};
+
+linkReportesMobile.onclick = (event) => {
   event.preventDefault();
 
   seccionPrincipal.classList.add("is-hidden");
