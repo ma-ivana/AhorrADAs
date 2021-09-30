ff3ac7fbed95d8a0e8e39a9abc91914b23f611c2
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-09-30 19:34:56 -0300

    Agrego seccion editar categorias y su funcionalidad

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const linkCategorias = document.querySelector(".link-categorias");
 const linkReportes = document.querySelector(".link-reportes");
 
 const seccionPrincipal = document.querySelector(".principal");
-const seccionCategorias = document.querySelector(".categorias");
+const seccionCategorias = document.querySelector("#seccion-categorias");
 const seccionReportes = document.querySelector(".reportes");
 
 linkBalance.onclick = (event) => {
@@ -27,6 +27,7 @@ linkReportes.onclick = (event) => {
   seccionPrincipal.classList.add("is-hidden");
   seccionCategorias.classList.add("is-hidden");
   seccionReportes.classList.remove("is-hidden");
+  seccionEditarCategoria.classList.add("is-hidden");
 };
 
 /*//////////////////// Menú desplegable mobile ////////////////////////*/
@@ -75,14 +76,12 @@ botonFiltros.onclick = (event) => {
   contenedorFiltros.classList.toggle("is-hidden");
   if (botonFiltros.textContent === "Ocultar filtros") {
     botonFiltros.textContent = "Mostrar filtros";
-    
   } else {
     botonFiltros.textContent = "Ocultar filtros";
-    
   }
 };
 
-////*Funcionalidad boton nueva operación */////
+/////////////////////*Funcionalidad boton nueva operación *//////////////////////////
 
 const seccionNuevaOperacion = document.getElementById(
   "seccion-nueva-operacion"
@@ -105,14 +104,39 @@ botonCancelarNuevasOperaciones.onclick = () => {
 
 ////* Boton agregar seccion Nuevas operaciones *////
 
-const botonAgregarNuevaOperacion = document.getElementById("boton-agregar-nuevas-operaciones")
-const contenedorListadoNuevasOperaciones = document.getElementById("contenedor-listado-nuevas-operaciones")
-const contenedorSinOperaciones = document.getElementById("contenedor-sin-operaciones")
-
+const botonAgregarNuevaOperacion = document.getElementById(
+  "boton-agregar-nuevas-operaciones"
+);
+const contenedorListadoNuevasOperaciones = document.getElementById(
+  "contenedor-listado-nuevas-operaciones"
+);
+const contenedorSinOperaciones = document.getElementById(
+  "contenedor-sin-operaciones"
+);
 
 botonAgregarNuevaOperacion.onclick = () => {
-  seccionNuevaOperacion.classList.add("is-hidden")
-  contenedorSinOperaciones.classList.add("is-hidden")
+  seccionNuevaOperacion.classList.add("is-hidden");
+  contenedorSinOperaciones.classList.add("is-hidden");
   seccionPrincipal.classList.remove("is-hidden");
-  contenedorListadoNuevasOperaciones.classList.remove("is-hidden")
+  contenedorListadoNuevasOperaciones.classList.remove("is-hidden");
+};
+
+////* Boton editar categorias *////
+
+const botonEditarCategoria = document.querySelector("#boton-editar-categoria");
+const seccionEditarCategoria = document.querySelector(
+  "#seccion-editar-categorias"
+);
+const botonCancelarEditarCategoria = document.querySelector("#boton-cancelar-editar-categoria")
+
+botonEditarCategoria.onclick = (event) => {
+  event.preventDefault();
+  seccionCategorias.classList.add("is-hidden");
+  seccionEditarCategoria.classList.remove("is-hidden");
+};
+
+botonCancelarEditarCategoria.onclick = (event) =>{
+ event.preventDefault();
+  seccionCategorias.classList.remove("is-hidden");
+  seccionEditarCategoria.classList.add("is-hidden");
 }
